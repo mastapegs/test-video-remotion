c1e22b622243e725ee5d36d811eae95bbf37ee04
Matthew Pagan <mastapegs01@gmail.com>
2021-10-01 16:59:52 -0400

    feat: configure eslint, prettier, and husky (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:prettier/recommended',
+		'@remotion',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'prettier'],
+	rules: {
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'import/prefer-default-export': 'off',
+		'react/jsx-filename-extension': ['error', {extensions: ['.tsx', '.jsx']}],
+		'prettier/prettier': 'error',
+	},
+};
