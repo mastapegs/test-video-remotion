15d5251ae3a5d8feda5faa1de406a9ceec2f9ea5
Matthew Pagan <mastapegs01@gmail.com>
2021-10-01 14:17:37 -0400

    feat: refactor styles

--- src/Love.tsx
+++ src/Love.tsx
@@ -16,39 +16,42 @@ export const Love: FC<LoveProps> = ({firstText, secondText}) => {
   const firstTextOpacity = interpolate(frame, [0, fadeInTime], [0, 1])
   const secondTextOpacity = interpolate(frame, [secondTextFrameIn, secondTextFrameIn + fadeInTime], [0, 1])
   
-  return (
-	<div style={{ 
+  const containerStyle = { 
     flex: 1, 
     backgroundColor: "black", 
     display: 'flex', 
     justifyContent: "center",
     alignContent: "center",
     fontFamily: "Arial, Helvetica, sans-serif"
-	}}
-	>
+	}
+
+  const firstTextStyle = {
+    color: "hsl(0, 100%, 70%)",
+    position: "absolute" as const,
+    top: 200,
+    left: 400,
+    fontSize: 200,
+    opacity: firstTextOpacity,
+  }
+
+  const secondTextStyle = {
+    color: "hsl(0, 100%, 70%)",
+    position: "absolute" as const,
+    top: 600,
+    left: 400,
+    fontSize: 200,
+    opacity: secondTextOpacity,
+  }
+  
+  return (
+	<div style={containerStyle}>
 		<div>
 			<Sequence from={0} durationInFrames={durationInFrames}>
-				<div style={{
-          color: "hsl(0, 100%, 70%)",
-          position: "absolute",
-          top: 200,
-          left: 400,
-          fontSize: 200,
-          opacity: firstTextOpacity,
-				}}
-				>{firstText}
+				<div style={firstTextStyle}>{firstText}
 				</div>
 			</Sequence>
 			<Sequence from={secondTextFrameIn} durationInFrames={durationInFrames - secondTextFrameIn}>
-				<div style={{
-          color: "hsl(0, 100%, 70%)",
-          position: "absolute",
-          top: 600,
-          left: 400,
-          fontSize: 200,
-          opacity: secondTextOpacity,
-				}}
-				>{secondText}
+				<div style={secondTextStyle}>{secondText}
 				</div>
 			</Sequence>
 		</div>
